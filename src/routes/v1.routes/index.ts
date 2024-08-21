4c992e47e0108f80f1cc8d36f39495ec72b6cfd2
Chimdie <nzeyoungwealth@gmail.com>
2024-08-21 22:59:38 +0100

    [feat]: adds routes to check username availability and get user by their username

--- src/routes/v1.routes/index.ts
+++ src/routes/v1.routes/index.ts
@@ -3,10 +3,9 @@ import { authRouter } from './auth.routes';
 import { userRouter } from './user.routes';
 import { authenticate } from '@/middleware/authenticate';
 import { product_router } from './product.routes';
-import { authorize_admin } from '@/middleware/auth.admin';
 
 export const routerV1 = Router();
 
 routerV1.use('/auth', authRouter);
-routerV1.use('/users', authenticate, authorize_admin, userRouter);
+routerV1.use('/users', userRouter);
 routerV1.use('/products', [authenticate], product_router);
